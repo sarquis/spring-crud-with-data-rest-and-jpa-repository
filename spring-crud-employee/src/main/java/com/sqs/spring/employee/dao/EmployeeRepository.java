428d5f7d2f11af072e809310565ad65d18baf692
rsnf <roberto.sarquis@gmail.com>
2023-07-23 14:30:58 -0300

    @RepositoryRestResource and spring.data.rest configurations

--- spring-crud-employee/src/main/java/com/sqs/spring/employee/dao/EmployeeRepository.java
+++ spring-crud-employee/src/main/java/com/sqs/spring/employee/dao/EmployeeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sqs.spring.employee.entity.Employee;
 
+// @RepositoryRestResource is only needed if you want to change default plural name.
+// FROM: /magic-api/employees TO: /magic-api/members 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
