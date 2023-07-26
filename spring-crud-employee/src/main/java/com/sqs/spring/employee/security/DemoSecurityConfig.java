ec11008ee0764bca0c54126161c144e7db627af7
rsnf <roberto.sarquis@gmail.com>
2023-07-26 16:35:13 -0300

    Spring Boot REST API Security - JDBC Authentication

--- spring-crud-employee/src/main/java/com/sqs/spring/employee/security/DemoSecurityConfig.java
+++ spring-crud-employee/src/main/java/com/sqs/spring/employee/security/DemoSecurityConfig.java
@@ -1,13 +1,13 @@
 package com.sqs.spring.employee.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,29 +15,9 @@ public class DemoSecurityConfig {
 
     @Bean
     // public no need
-    InMemoryUserDetailsManager userDetailsManager() {
+    JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
 
-	// @formatter:off
-	UserDetails john = User.builder()
-		.username("john")
-		.password("{noop}test123")
-		.roles("EMPLOYEE")
-		.build();
-
-	UserDetails mary = User.builder()
-		.username("mary")
-		.password("{noop}test123")
-		.roles("EMPLOYEE", "MANAGER")
-		.build();
-
-	UserDetails susan = User.builder()
-		.username("susan")
-		.password("{noop}test123")
-		.roles("EMPLOYEE", "MANAGER", "ADMIN")
-		.build();
-	// @formatter:on
-
-	return new InMemoryUserDetailsManager(john, mary, susan);
+	return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
